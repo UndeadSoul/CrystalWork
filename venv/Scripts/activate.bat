bacf748a5c48b8d2f10a6477ec6ddfebf02c6332
UndeadSoul <noestoymuertobro@gmail.com>
2026-08-19 12:34:48 -0400

    basic setup

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\diego\Documents\Proyectos\CrystalWork\CrystalWork\venv
+set VIRTUAL_ENV=E:\Projects\CrystalWork\CrystalWork\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
